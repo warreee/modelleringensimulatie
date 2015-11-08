1c1286006e6689f5dd0fca1dfc4d5a1be858aaa4
warreee <ward.schodts@student.kuleuven.be>
2015-11-08 13:02:30 +0100

    opgave1

---
+++ oefenzitting1/qrgivens.m
@@ -0,0 +1,21 @@
+function [ Q,R ] = qrgivens( A )
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+  [m,n] = size(A);
+  Q = eye(m);
+  R = A;
+
+  for j = 1:n
+    for i = m:-1:(j+1)
+      G = eye(m);
+      temp = givens( R(i-1,j),R(i,j));
+      c = temp(1,1);
+      s = temp(1,2);
+      G([i-1, i],[i-1, i]) = [c -s; s c];
+      R = G'*R;
+      Q = Q*G;
+    end
+  end
+
+end
+
