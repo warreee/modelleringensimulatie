ad657487d76d9c7b7684b4d9d2a39617246f57a6
warreee <wardschodts@hotmail.com>
2015-11-09 12:22:32 +0100

    oefenzitting 2

---
+++ oefenzitting2/oef7.m
@@ -0,0 +1,20 @@
+load('Agrigento.mat');
+
+[U,S,V] = svd(Z);
+colormap gray
+
+rel_error = [];
+
+
+for k = 10 : 10 : 200
+    S(1:k,1:k);
+    temp = U(:,1:k) * S(1:k,1:k) * transpose(V(:,1:k));
+    rel = norm(Z - temp)/norm(Z);
+    rel_error = [rel_error, rel];
+    %figure;
+    %imagesc(temp);
+   % waitforbuttonpress;
+end
+
+semilogy(rel_error)
+
