094584c8b0bd18d51d5413b5663d99b89bfddf48
warreee <ward.schodts@student.kuleuven.be>
2015-11-26 13:35:35 +0100

    opdracht 4

--- practicum1/opdrachten.m
+++ practicum1/opdrachten.m
@@ -4,3 +4,11 @@ load('AmazonBookReviews.mat');
 spy(Ratings);
 
 %% Opdracht 4
+
+[Predicted, err] = r0381767_predictedRatings(Ratings, 15,10000);
+figure
+loglog(err)
+title('Relative error in function of # of iterations')
+xlabel('# of iterations')
+ylabel('Relative error')
+% print -depsc opdracht4 %used for output
--- practicum1/vraag4.m
+++ practicum1/vraag4.m
@@ -1,4 +1,2 @@
-load('AmazonBookReviews.mat')
-tic;
+
 [Predicted, err] = r0381767_predictedRatings(Ratings, 15,10000);
-toc;
