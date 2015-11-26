90c6fd6a538b81160d85523d3dcfab44de3b8d7f
warreee <ward.schodts@student.kuleuven.be>
2015-11-26 23:18:58 +0100

    rename of helper error function

--- practicum1/r0381767_predictedRatings.m
+++ practicum1/r0381767_predictedRatings.m
@@ -1,5 +1,8 @@
 function [ Predicted, err ] = r0381767_predictedRatings( Ratings, r, kappa )
 %Low rank approximation for a uncomplete matrix Ratings
+%   Predicted is the predicted approximation
+%   err is a vector with the approximation error in every step (kappa steps
+%   long)
 
 [m,n] = size(Ratings);
 one = ones(m,n);
@@ -7,13 +10,13 @@ D = zeros(m,n);
 D(Ratings > 0) = 1;
 
 Ri = D.*Ratings + 1.5*(one - D);
-Ak = [];
-err = [];   
+
+err = zeros(1,kappa);   
 
 for i = 1:kappa
     [U,S,V] = svd(Ri);
     Ak = U(:,1:r) * S(1:r,1:r) * transpose(V(:,1:r));
-    err = [err,r0381767_approximationError(Ratings, Ak)];
+    err(i) = r0381767_approximationError(Ratings, Ak);
     Ri = D.*Ratings + (one - D).*Ak;
 end
 rank(Ak)
