b72185f20802f6802cd12d465dc953d33f402f5f
warreee <ward.schodts@student.kuleuven.be>
2015-11-26 12:54:45 +0100

    opdracht 1

--- practicum1/opdrachten.m
+++ practicum1/opdrachten.m
@@ -1,19 +1,6 @@
 %% Opdracht 1
 
 load('AmazonBookReviews.mat');
-%spy(Ratings);
+spy(Ratings);
 
-%% Opdracht 2 
-
-function [ err ] = r0381767_approximationError( Ratings, Predicted )
-% Summary of this function goes here
-%   Detailed explanation goes here
-
-D = binaryMartix(Ratings);
-
-teller = sum(sum((Ratings-(D.*Predicted)).^2));
-
-noemer = sum(sum((Ratings).^2));
-
-err = sqrt(teller/noemer);
-end
+%% Opdracht 4
