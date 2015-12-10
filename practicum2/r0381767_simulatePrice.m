0b3d49ef8eb07a247ce1fd2ead78b4580df39071
warreee <wardschodts@hotmail.com>
2015-12-10 18:04:18 +0100

    oef1 af

--- practicum2/r0381767_simulatePrice.m
+++ practicum2/r0381767_simulatePrice.m
@@ -2,6 +2,11 @@ function [ path ] = r0381767_simulatePrice( initialPrice, days, mu, sigma )
 
 path = zeros(1,days+1);
 
+path(1) = initialPrice;
+
+for t = 1:(days+1)
+    path(t+1) = path(t)*exp((mu-0.5*sigma^2) + sigma*randn);
+end
 
 end
 
