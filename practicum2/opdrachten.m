8256861ad86f81fa1b69ed8837e5bf12fb316b9e
warreee <wardschodts@hotmail.com>
2015-12-19 18:26:00 +0100

    opdracht 15

--- practicum2/opdrachten.m
+++ practicum2/opdrachten.m
@@ -40,8 +40,23 @@ r0381767_upstreamEarnings(45)
 
 %% Opdracht 9
 
+%% Opdracht 12
 
+load('Debt.mat')
 
+sum(Debt(:,3));
 
+%% Opdracht 14
 
+interest = zeros(360,1);
+principal = zeros(360,1);
+for m = 1 : 360
+    [interest(m),principal(m)] = r0381767_interestExpense(Debt, m);
+end
 
+%% Opdracht 15
+
+plot(cumsum(interest), 'x-');
+hold all
+plot(cumsum(principal), 'o-');
+legend('Interest', 'Hoofdsom', 'Location', 'NorthWest');
