7305866d663191e5e2a6eb94fa96676c2d7206b2
warreee <wardschodts@hotmail.com>
2015-12-19 18:26:34 +0100

    interestexpense

---
+++ practicum2/r0381767_interestExpense.m
@@ -0,0 +1,21 @@
+function [ interest, principal ] = r0381767_interestExpense( Debt, month )
+% Calculates the interest and the principal that in that month has to be
+% payed.
+
+n = size(Debt,1);
+
+interest = 0;
+principal = 0;
+
+for i = 1 : n
+    if mod(Debt(i,1),12) == mod(month,12) && month < Debt(i,1)
+        interest = interest + (Debt(i,4)/100)*Debt(i,3);
+    end
+    if Debt(i,1) == month
+        principal = principal + Debt(i,3);
+    end
+end
+
+
+end
+
