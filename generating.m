7fb979ffbe0e3cf31e71db739338a092ab5b912c
warreee <wardschodts@hotmail.com>
2016-01-07 19:02:16 +0100

    some working

--- generating.m
+++ generating.m
@@ -38,3 +38,29 @@ mean(Z1)
 std(Z1)
 mean(Z2)
 std(Z2)
+
+%%
+
+qrTimes = zeros(20,1);
+svdTimes = zeros(20,1);
+for i = 1 : 20
+   A = randn(i*100);
+   tic;
+   [Q,R,P] = qr(A,0);
+   qrTimes(i) = toc;
+   tic;
+   [U,S,V] = svd(A,'econ');
+   svdTimes(i) = toc;
+end
+plot(qrTimes); hold all; plot(svdTimes);
+
+%%
+A = [ 0.5, 0.5; 0.35, 0.65];
+A
+for i = 1 : 50
+    i
+    A = A * A 
+end
+
+%%
+A= [-14.487757 -8.259503 -10.622702 16.7520
